6ecdfa18165f754c59a0e30fce5b263f207ceee0
Lester Laurence Z. Corpuz <lester19.corpuz28@gmail.com>
2022-01-03 14:35:25 +0800

    Get quote button added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -28,21 +29,45 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<String> quotes = [
+    "Quote 1",
+    "Quote 2",
+    "Quote 3",
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    List<String> authorNames = ["Name1", "Name2", "Name3", "Name4", "Name5"];
-    return ListView(
-      children: List.generate(
-        5,
-        (index) => ListTile(
-          title: Text("$index. " + authorNames[index]),
-          dense: true,
-          onTap: () => buildText(context),
-        ),
+    return Stack(children: [
+      Column(
+        children: [
+          buildButton("name", 1),
+          buildButton("name", 1),
+          buildButton("name", 1),
+          buildButton("name", 1),
+          buildButton("name", 1),
+        ],
       ),
-    );
+    ]);
   }
 }
+
+Widget buildButton(name, order) => ExpansionTile(
+      title: Text("$order. " + name),
+      children: [
+        ListTile(
+            trailing: Text('Generate Quote!'),
+            onTap: () => _showMyDialog() //print("quote should appear"),
+            )
+      ],
+    );
+
+//generateNewRand () => ;
+
